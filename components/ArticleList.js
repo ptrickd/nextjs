3c6b64581ee2427997857f4e36e526d4532109ef
ptrickd <patrickdion8@gmail.com>
2021-05-14 15:25:16 -0600

    meta component

--- components/ArticleList.js
+++ components/ArticleList.js
@@ -4,7 +4,7 @@ import articleStyles from '../styles/Article.module.css'
 const ArticleList = ({ articles }) => {
     return (
         <div className={articleStyles.grid}>
-            {articles.map(article => <ArticleItem article={article} />)}
+            {articles.map(article => <ArticleItem article={article} key={article.id} />)}
         </div>
     )
 }
--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,10 @@
+import Meta from '../components/Meta'
 import Head from 'next/head'
 
 const about = () => {
     return (
         <div>
-            <Head>
-                <title>About</title>
-            </Head>
+            <Meta title='About' />
             <h1>About</h1>
         </div>
     )
